eb61a9e3e41889aeeb020fb84de3844a1155c77f
gudkovWay <gudkoviurii@gmail.com>
2024-05-09 01:09:01 +0300

    🤠 fix: remove any

--- src/elements/Ads/Filter.tsx
+++ src/elements/Ads/Filter.tsx
@@ -1,12 +1,11 @@
 import { Typography, Button } from "@mui/material"
-import { Stack } from "@mui/system"
+import { Stack, SxProps } from "@mui/system"
 import Icon from "../../components/Icon"
 import { allButtonStyles, containerStyles, otherButtonStyles, titleStyles } from "./styles";
 
-// TODO: remove any
-const FilterButton: React.FC<{ icon: 'all' | 'rewards' | 'results' | 'movie', text: string, styles: any }> = ({ icon, text, styles }) => (
+const FilterButton: React.FC<{ icon: 'all' | 'rewards' | 'results' | 'movie', text: string, styles: SxProps, color?: string }> = ({ icon, text, styles, color }) => (
   <Button startIcon={<Icon name={icon} />} variant="text" sx={styles} disabled>
-    <Typography variant="body1" sx={{color: styles.color}}>{text}</Typography>
+    <Typography variant="body1" sx={{ color: color }}>{text}</Typography>
   </Button>
 );
 
@@ -17,10 +16,10 @@ const Filter = () => {
         Фильтр ленты
       </Typography>
       <Stack direction="column" sx={{ width: "100%", gap: "12px", padding: '0', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-        <FilterButton icon="all" text="Все" styles={allButtonStyles} />
-        <FilterButton icon="results" text="Результаты соревнований" styles={otherButtonStyles} />
-        <FilterButton icon="movie" text="Видео" styles={otherButtonStyles} />
-        <FilterButton icon="rewards" text="Достижения" styles={otherButtonStyles} />
+        <FilterButton icon="all" text="Все" styles={allButtonStyles} color={allButtonStyles.color} />
+        <FilterButton icon="results" text="Результаты соревнований" styles={otherButtonStyles} color={otherButtonStyles.color} />
+        <FilterButton icon="movie" text="Видео" styles={otherButtonStyles} color={otherButtonStyles.color} />
+        <FilterButton icon="rewards" text="Достижения" styles={otherButtonStyles} color={otherButtonStyles.color} />
       </Stack>
     </Stack>
   );
